33791462de48e45da848576a1252019c44e17064
khamidullo <khkhamidullo@gmail.com>
2025-04-04 12:36:28 +0500

    update

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,30 +5,30 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  // @Get()
+  // getData() {
+  //   return this.appService.getData();
+  // }
+
+  // @Get('save')
+  // saveMockData() {
+  //   return this.appService.saveMockData();
+  // }
+
   @Get()
-  getData() {
-    return this.appService.getData();
-  }
-
-  @Get('save')
-  saveMockData() {
-    return this.appService.saveMockData();
-  }
-
-  @Get('report')
   @Render('index')
   summarized() {
     return this.appService.summarized();
   }
 
-  @Get('remove')
-  removeMockData() {
-    return this.appService.removeMockData();
-  }
+  // @Get('remove')
+  // removeMockData() {
+  //   return this.appService.removeMockData();
+  // }
 
-  @Get('ejs')
-  @Render('index')
-  root() {
-    return this.appService.summarized();
-  }
+  // @Get('ejs')
+  // @Render('index')
+  // root() {
+  //   return this.appService.summarized();
+  // }
 }
